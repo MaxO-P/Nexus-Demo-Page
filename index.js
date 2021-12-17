8c848387613f020c9b71f2fadea1370fa40c8876
Mistakd <maxwellolinskypaul@cvsdvt.org>
2021-12-17 13:25:35 -0500

    pretty neat
    cbt

---
+++ index.js
@@ -0,0 +1,12 @@
+let sheet = document.getElementById("sheet");
+function cycle() {
+    sheet.setAttribute("href", "style2.css");
+}
+
+function cycleNext() {
+    sheet.setAttribute("href", "style3.css");
+}
+
+function cycleBack() {
+    sheet.setAttribute("href", "style.css");
+}
