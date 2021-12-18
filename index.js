5078b6973b4958b96e71ee8f4aa55c40469ac2fd
Windzyboy <63932310+Windzyboy@users.noreply.github.com>
2021-12-18 02:28:51 -0500

    ready for pull request

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const mainsheet = document.getElementById("mainsheet");
 const templates = document.getElementById("sheets");
-var sheets = templates.content.children;
+const prev = document.getElementById("pButton");
+const next = document.getElementById("nButton");
+var sheets = Array.from(templates.content.children);
+console.log(sheets)
 var sheetIndex = 0;
 
 /**
  * Cycles through the sheets
  * @param {boolean} [isBackwards] - if true, the sheet index will be moved backwards.
  */
-function cycle(isBackwards) {
-    sheetIndex += isBackwards ? -1 : 1;
-    if (sheetIndex < 0) sheetIndex = sheets.length - 1; else if (sheetIndex >= sheets.length) sheetIndex = 0;
+function cycle(isBackwards = false) {
+    // This is 2:00 AM code. I'm sorry.
+    sheetIndex += (isBackwards ? -1 : 1);
+    if (sheetIndex < 0) {
+        sheetIndex = sheets.length - 1;
+    } else if (sheetIndex >= sheets.length) {
+        sheetIndex = 0;
+    }
     mainsheet.href = sheets[sheetIndex].href;
 }
 
-console.log(sheets);
+prev.onclick = () => { cycle(true); };
+next.onclick = () => { cycle(false); };
